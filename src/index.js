606be0b130e46a2121062aaa0ae48b991970beb6
makar1227 <makar1227@gmail.com>
2018-03-04 20:19:53 +0300

    res

--- src/index.js
+++ src/index.js
@@ -1,27 +1,49 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.expression = [initialValue];
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        this.expression.push("+");
+        this.expression.push(number);
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.expression.push("-");
+        this.expression.push(number);
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.expression.push("*");
+        this.expression.push(number);
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number) {
+        this.expression.push("/");
+        this.expression.push(number);
+        return this;
+    }
+
+    pow(number) {
+        let val = this.expression.pop();
+        if(typeof(val)=="string" && val.includes("Math.pow")){
+            let tempLength = val.indexOf(")")-(val.indexOf(",")+1);
+            let temp = val.substr(val.indexOf(",")+1,tempLength);
+            number=Math.pow(temp, number);
+            tempLength= val.indexOf(",")-(val.indexOf("(")+1);
+            val = val.substr(val.indexOf("(")+1,tempLength);
+        }
+        this.expression.push("Math.pow("+val+","+number+")");
+        return this;
+    }
+    valueOf() {
+        console.log("RESULT = " + this.expression)
+        this.expression = eval(this.expression.join(""));
+        return this.expression;}
 }
 
 module.exports = SmartCalculator;
+
